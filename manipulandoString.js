ddb5130f7b5aa0ee1ee0b39e36cc8f83f584f9f6
NatanBarboza <natanbarboza084@gmail.com>
2022-05-15 23:15:00 -0300

    adição de novos métodos

--- manipulandoString.js
+++ manipulandoString.js
@@ -2,3 +2,7 @@ const string = "It's a string for my test.";
 
 console.log(string.length)
 console.log(string.slice(0, 5))
+console.log(string.indexOf("It's"))
+console.log(string.concat("Test"))
+console.log(string.toLowerCase())
+console.log(string.toUpperCase())
